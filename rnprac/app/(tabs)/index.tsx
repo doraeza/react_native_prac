2dc849f24f194a86e194f382f48cfa96fbcf815c
doraeza <pp8960@naver.com>
2023-05-15 23:13:31 +0900

    modify main3

--- rnprac/app/(tabs)/index.tsx
+++ rnprac/app/(tabs)/index.tsx
@@ -7,6 +7,8 @@ export default function TabOneScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Main</Text>
+      <Text style={styles.title}>Main2</Text>
+
       {/* <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" /> */}
       {/* <EditScreenInfo path="app/(tabs)/index.tsx" /> */}
     </View>
