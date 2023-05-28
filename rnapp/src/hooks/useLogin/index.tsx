66b632aeae07e07e3071387c9ada17b09e0c13f0
doraeza <pp8960@naver.com>
2023-05-28 21:24:02 +0900

    login2

--- rnapp/src/hooks/useLogin/index.tsx
+++ rnapp/src/hooks/useLogin/index.tsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react"
 import axios from "axios"
 import useInput from "../common/useInput"
+import { NativeEvent } from "react-native-reanimated/lib/types/lib/reanimated2/commonTypes"
+import { NativeMouseEvent } from "react-native/types"
 
 function useLogin() {
 
@@ -16,7 +18,7 @@ function useLogin() {
     //         message.error("에러가 발생했습니다. 관리자에게 문의해주세요.")
     //     }
     // }
-    const login = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const login = (e?: NativeMouseEvent) => {
         console.log("api create")
         // axios.post("http://localhost:8080/scenario/post", {}).then((res) => {
         //     console.log(res)
