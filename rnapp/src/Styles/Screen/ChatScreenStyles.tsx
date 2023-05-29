a10ad4ffd32e6b98d5b1a67cd9ba2fc76b85b1f4
doraeza <pp8960@naver.com>
2023-05-29 20:41:07 +0900

    style default

---
+++ rnapp/src/Styles/Screen/ChatScreenStyles.tsx
@@ -0,0 +1,24 @@
+import {StyleSheet} from 'react-native';
+
+export const ScreenStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 30,
+    padding: 8,
+  },
+  input: {
+    width: '70%',
+    borderWidth: 1,
+    borderColor: 'black',
+    padding: 10,
+    marginRight: 30,
+  },
+  bottomContainer: {
+    marginHorizontal: 10,
+    flexDirection: 'row',
+    marginBottom: 80,
+  },
+  list: {
+    height: '80%',
+  },
+});
