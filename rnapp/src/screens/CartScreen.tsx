ca42760177f55bdf295e5efe743b2d91ef0b6076
doraeza <pp8960@naver.com>
2023-06-05 16:40:23 +0900

    cart redux dispatch3

--- rnapp/src/screens/CartScreen.tsx
+++ rnapp/src/screens/CartScreen.tsx
@@ -2,14 +2,36 @@ import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import Test from '../components/common/TestModule'
 import {ScreenStyles} from '../Styles/Screen/ChatScreenStyles'
+import { useSelector } from 'react-redux'
+import { AppState } from '../interface/AppState'
 
 const CartScreen =()=> {
+  const onCart = useSelector((state: AppState) => state.onCart);
+
+  const renderCartList =(onCart:any)=> {
+    console.log("onCart ==>", onCart)
+    return (
+      <View>
+        {onCart && onCart.map((
+          el:{
+            itemIdx: number;
+            itemName: string;
+            itemQuantity: number;
+            })=>
+        {
+          <View>
+          <Text>name : {el['itemName']}</Text>
+          <Text>ea : {el['itemQuantity']}</Text>
+          </View>
+        })}
+      </View>
+    )
+
+  }
+  
     return (
       <View style={ScreenStyles.container}>
-        <Text>Add friends here!</Text>
-        <Text>Open up App.tsx to staaart working on your app!</Text>
-        
-        <Test />
+        {renderCartList(onCart)}
       </View>
     );
 }
